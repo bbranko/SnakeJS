edaf943c7e7824741c5b7d25d8b3bcdf67a901ca
Branko Bavrljic <kobran.darthen+github@gmail.com>
2015-02-28 17:39:33 +0100

    refactoring final step, game control object and wraper created

--- js/snake/snake.js
+++ js/snake/snake.js
@@ -1,21 +1,6 @@
 //"use strict";
 
-var snakejs = function () {
-    //Canvas stuff
-    var canvas = document.getElementById("canvas");
-    var gw = 45;
-    var gh = 45;
-
-    var food;
-    var score;
-    var keycode;
-    var game_on = false;
-    var game_speed = 60;
-    var game_loop;
-
-    var menu = (function () {
-
-    }());
+var snakejs = (function () {
 
     // all hail snake control object!
     var snake = (function () {
@@ -24,9 +9,9 @@ var snakejs = function () {
         var is_alive;
 
         function init(length) {
-            is_alive = true; //raise da snake from the grips of dooom
-            direction = "right"; //also set default direction of movement
             length = length || 5; //default length of the snake
+            direction = "right"; //also set default direction of movement
+            is_alive = true; //raise da snake from the grips of dooom
 
             snake_array = []; //Empty array to start with
             for (var i = length - 1; i >= 0; i--) {
@@ -54,7 +39,7 @@ var snakejs = function () {
             //This will restart the game if the snake hits the wall
             //Lets add the code for body collision
             //Now if the head of the snake bumps into its body, the game will restart
-            if (check_border_collision(nx, ny) || check_collision(nx, ny, snake_array)) {
+            if (game.check_collision_vs_border(nx, ny) || game.check_collision_vs_array(nx, ny, snake_array)) {
                 is_alive = false;
                 return;
             }
@@ -63,11 +48,11 @@ var snakejs = function () {
             //The logic is simple
             //If the new head position matches with that of the food,
             //Create a new head instead of moving the tail
-            if (check_collision(nx, ny, [food])) {
+            if (game.check_collision_vs_array(nx, ny, [game.get_food_coords()])) {
                 var tail = {x: nx, y: ny};
-                score++;
+                game.inc_score();
                 //Create new food
-                create_food();
+                game.create_food();
             }
             else {
                 var tail = snake_array.pop(); //pops out the last cell
@@ -152,14 +137,14 @@ var snakejs = function () {
         }
 
         //fixme: add out of bounds draw prevention
-        function paint_text(text, x, y){
-            if(!text) return;
+        function paint_text(text, x, y) {
+            if (!text) return;
 
             x = x || 1;
             y = y || 1;
 
-            if(x<0) x += w;
-            if(y<0) y += h;
+            if (x < 0) x += w;
+            if (y < 0) y += h;
 
             ctx.fillText(text, x, y);
         }
@@ -173,115 +158,178 @@ var snakejs = function () {
     }());
 
     var game = (function () {
-        //playground
-        //menu
-        //snake
-        //run
-    });
+        var old_document_keydown = null;
+        var game_canvas;
+        var game_width;
+        var game_height;
 
-    function init() {
-        playground.init(canvas, gw, gh);
-        snake.init(5);
-        create_food(); //Now we can see the food particle
-        //finally lets display the score
-        score = 0;
+        var food = {x: 0, y: 0}; //init food object
+        var score;
+        var keycode;
+        var game_on;
+        var game_speed;
+        var game_loop;
 
-        //initial paint
-        game_on = false;
-        if (typeof game_loop != 'undefined') clearInterval(game_loop);
-        paint();
-    }
+        //note: game width height represents number of playground pixels and not actual canvas px size
+        //canvas size is defined beforehand and the game is rendered within it
+        function create_new(canvas, gw, gh, gs) {
+            if (!canvas || !canvas.getContext('2d')) {
+                end();
+                throw 'Canvas for the SnakeJS game can NOT be undefined!';
+            }
 
-    init();
+            game_canvas = canvas;
+            game_width = gw || 45;
+            game_height = gh || 45;
+            game_speed = gs || 60;
 
+            //Lets add the keyboard controls now, via chaining!
+            if (!old_document_keydown) {
+                old_document_keydown = document.onkeydown;
+                document.onkeydown = function (e) {
+                    keyboard_controller(e);
+                    if (old_document_keydown) old_document_keydown(e);
+                };
+            }
 
-    //Lets create the food now
-    function create_food() {
-        food = {
-            x: Math.round(Math.random() * gw - 1),
-            y: Math.round(Math.random() * gh - 1)
-        };
-        //This will create a cell within playground boundaries
-        //note: playground coords go from 0 x 0 to gw x gh
-    }
-
-    //Lets paint the snake now
-    function paint() {
-        //To avoid the snake trail we need to paint the BG on every frame
-        playground.paint();
-
-        //move snake
-        snake.move();
-        //if it survives,
-        if (snake.isAlive()) {
-            //game continues
-            snake.paint()
-        } else {
-            //otherwise, game over
-            init();
-            return;
+            restart()
         }
 
-        //Lets paint the food
-        playground.paint_cell(food.x, food.y);
-        //Lets paint the score
-        var score_text = 'Score: ' + score;
-        var keycode_text = 'Keycode: ' + keycode;
-        playground.paint_text(score_text, 5, -5);
-        playground.paint_text(keycode_text, 5, -15);
-    }
+        function restart() {
+            game_on = false;
+            playground.init(game_canvas, game_width, game_height);
+            snake.init(5);
+            create_food(); //Now we can see the food particle
+            //finally lets display the score
+            score = 0;
 
-
-    function check_collision(x, y, array) {
-        //This function will check if the provided x/y coordinates exist
-        //in an array of cells or not
-        for (var i = 0; i < array.length; i++) {
-            if (array[i].x == x && array[i].y == y)
-                return true;
+            //initial paint
+            if (typeof game_loop != 'undefined') clearInterval(game_loop);
+            paint();
         }
-        return false;
-    }
 
-    function check_border_collision(x, y) {
-        return x === -1 || x === gw || y === -1 || y === gh;
-    }
+        function end() {
+            //restore page's keydown
+            if (!!old_document_keydown) {
+                document.onkeydown = old_document_keydown;
+            }
 
-    //Lets add the keyboard controls now, via chaining!
-    old_document_keydown = document.onkeydown;
-    document.onkeydown = function (e) {
-        snake_keyboard_controller(e);
-        if (old_document_keydown) old_document_keydown(e);
-    };
+            if (typeof game_loop != 'undefined') clearInterval(game_loop);
+        }
 
-    //The snake is now keyboard controllable
-    function snake_keyboard_controller(e) {
-        var key = e.which;
-        keycode = key;
+        //Lets paint the snake now
+        function paint() {
+            //To avoid the snake trail we need to paint the BG on every frame
+            playground.paint();
 
-        //space for pause
-        if (key == '32') {
-            //Lets move the snake now using a timer which will trigger the paint function
-            //every 60ms
+            //move snake
+            snake.move();
+            //if it survives,
+            if (snake.isAlive()) {
+                //game continues
+                snake.paint()
+            } else {
+                //otherwise, game over
+                restart();
+                return;
+            }
+
+            //Lets paint the food
+            playground.paint_cell(food.x, food.y);
+            //Lets paint the score
+            var score_text = 'Score: ' + score;
+            var keycode_text = 'Keycode: ' + keycode;
+            playground.paint_text(score_text, 5, -5);
+            playground.paint_text(keycode_text, 5, -15);
+        }
+
+        //desired game keyboard controls
+        function keyboard_controller(e) {
+            var key = e.which;
+            keycode = key;
+
+            //space for pause
+            if (key == '32') {
+                //Lets move the snake now using a timer which will trigger the paint function
+                //every 60ms
+                if (game_on) {
+                    game_on = false;
+                    clearInterval(game_loop);
+                }
+                else {
+                    game_on = true;
+                    game_loop = setInterval(paint, game_speed);
+                }
+            }
+
             if (game_on) {
-                game_on = false;
-                clearInterval(game_loop);
-            }
-            else {
-                game_on = true;
-                game_loop = setInterval(paint, game_speed);
+                // suicide prevention is integrated into snake.setDirection
+                if (key == '37') snake.setDirection('left');
+                else if (key == '38') snake.setDirection('up');
+                else if (key == '39') snake.setDirection('right');
+                else if (key == '40') snake.setDirection('down');
             }
         }
 
-        if (game_on) {
-            // suicide prevention is integrated into snake.setDirection
-            if (key == '37') snake.setDirection('left');
-            else if (key == '38') snake.setDirection('up');
-            else if (key == '39') snake.setDirection('right');
-            else if (key == '40') snake.setDirection('down');
+        function create_food() {
+            //reuse already initialized food object
+            food.x = Math.round(Math.random() * game_width - 1);
+            food.y = Math.round(Math.random() * game_height - 1);
+            //This will reposition a cell within playground boundaries
+            //note: playground coords go from 0 x 0 to game_width x game_height -1
         }
-    }
 
-};
+        function get_food_coords() {
+            //deep copy so to protect food object \m/
+            return {
+                x: food.x,
+                y: food.y
+            }
+        }
+
+        function inc_score() {
+            score += 1;
+        }
+
+
+        function check_collision_vs_array(x, y, array) {
+            //This function will check if the provided x/y coordinates exist
+            //in an array of cells or not
+            for (var i = 0; i < array.length; i++) {
+                if (array[i].x == x && array[i].y == y)
+                    return true;
+            }
+            return false;
+        }
+
+        function check_collision_vs_border(x, y) {
+            return x === -1 || x === game_width || y === -1 || y === game_height;
+        }
+
+        return {
+            'create_new': create_new,
+            'restart': restart,
+            'end': end,
+            'check_collision_vs_array': check_collision_vs_array,
+            'check_collision_vs_border': check_collision_vs_border,
+            'get_food_coords': get_food_coords,
+            'create_food': create_food,
+            'inc_score': inc_score
+        }
+
+    }());
+
+    //return game control object, this could be its own module, but nah...
+    return (function () {
+        var newGame = game;
+        return {
+            'create_new': newGame.create_new,
+            'restart': newGame.restart,
+            'end': newGame.end
+        }
+    }());
+
+}());
 
 /*
  move intention
